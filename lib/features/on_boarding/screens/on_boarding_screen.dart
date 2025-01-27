9818a694e14b292257fe66792e690f359ecb6e14
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-27 12:13:39 +0200

    Part 4 design on Boarding Page Part 1 add Orbitron Font Family

--- lib/features/on_boarding/screens/on_boarding_screen.dart
+++ lib/features/on_boarding/screens/on_boarding_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/core/resources/alignment_managers.dart';
+import 'package:music_app/core/resources/assets_managers.dart';
+import 'package:music_app/core/resources/color_managers.dart';
+import 'package:music_app/core/resources/font_managers.dart';
+import 'package:music_app/core/resources/height_values_managers.dart';
+import 'package:music_app/core/resources/width_values_managers.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment(AlignmentManagersX.x0_20, AlignmentManagersY.y_0_98),
+                end: Alignment(AlignmentManagersX.x_0_20, AlignmentManagersY.y0_98),
+                colors: [
+                  ColorManagers.kPrimaryColor,
+                  ColorManagers.kSecondPrimaryColor.withValues(alpha: 0.54),
+                ])),
+        child:  Center(
+          child: Text("Hello", style: TextStyle(fontFamily: FontManagers.orbitronName),),
+        ),
+      ),
+    );
   }
 }
