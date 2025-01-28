f724183dba70dd1fb3483a935db75b774e8dc280
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-28 13:21:54 +0200

    Part 11 design Home Page Part 2 Navigation Bar icons

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -5,6 +5,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: [
+      BottomNavigationBarItem(
+        label: "",
+        icon: Icon(Icons.align_horizontal_left),
+      ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Icon(Icons.list),
+          ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.settings),
+              ),
+    ]));
   }
 }
