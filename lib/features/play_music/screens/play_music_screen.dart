d1a71c9dc3fac4b209f8ad7f050d51c0ca012056
Atlas Home <alhefnawimoh@hotmail.com>
2025-01-31 14:12:38 +0200

    Part 40 Logic Part 5 Add AudioPlayer Package

--- lib/features/play_music/screens/play_music_screen.dart
+++ lib/features/play_music/screens/play_music_screen.dart
@@ -2,18 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:music_app/controller/home_page_controller.dart';
 import 'package:music_app/core/resources/alignment_managers.dart';
 import 'package:music_app/core/resources/color_managers.dart';
+import 'package:music_app/core/resources/constants_value.dart';
 import 'package:music_app/features/play_music/widgets/custom_tool_2_play_music.dart';
 import 'package:music_app/features/play_music/widgets/custom_app_bar_play_music_screen.dart';
 import 'package:music_app/features/play_music/widgets/custom_button_controller_play_music.dart';
 import 'package:music_app/features/play_music/widgets/custom_songs_details_play_music_screen.dart';
 import 'package:music_app/features/play_music/widgets/custom_tools_play_music.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class PlayMusicScreen extends StatelessWidget {
+class PlayMusicScreen extends StatefulWidget {
+
   const PlayMusicScreen({super.key});
 
   @override
+  State<PlayMusicScreen> createState() => _PlayMusicScreenState();
+}
+
+class _PlayMusicScreenState extends State<PlayMusicScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+  }
+  int? index;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+     index =  ModalRoute.of(context)!.settings.arguments as int;
+
+     play();
+
+  }
+  void play ()async{
+    AudioCache audioCache = AudioCache(prefix: "");
+    Uri uri = await audioCache.load(ConstantsValue.listQuran[index!].pathSong);
+    AudioPlayer audioPlayer = AudioPlayer();
+    audioPlayer.play(UrlSource(uri.toString()));
+  }
+  @override
   Widget build(BuildContext context) {
-   int index =  ModalRoute.of(context)!.settings.arguments as int;
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBarPlayMusicScreen(
